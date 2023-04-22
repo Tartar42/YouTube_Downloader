6e2b29a0e591837b7c8b2d2d23e2c23d5987ce72
Tarsila <tarsila@alatazan.com>
2023-04-22 19:38:27 +0200

    First Commit

---
+++ youtubeDownloader.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+from sys import argv
+
+#way we are going to access the link from the command line in a python code is using this:
+link = argv[1]
+# argv takes all the things you input in the command line when you are running this programm. The first argument is
+# allways the name of the programm ( argv[0] ). argv[1] = first command  line in an argument which we give when we
+# are on this programm
+yt = YouTube(link)
+# creates YouTube-Object from this link 
+
+print("Title: ", yt.title)
+
+print("Views: ", yt.views)
+
+# tipp: when you run python3 youtubeDownloader.py yt."(link)", make sure of the "yt"!!
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download("C:\Users\tarsi\Downloads\YoutTubeDownloads")
