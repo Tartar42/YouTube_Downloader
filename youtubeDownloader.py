e8e9f85edf7895140e211eb9e283fcc25d92350a
Tarsila <tarsila@alatazan.com>
2023-04-23 07:59:53 +0200

    Still not working

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -10,11 +10,25 @@ yt = YouTube(link)
 # creates YouTube-Object from this link 
 
 print("Title: ", yt.title)
-
-print("Views: ", yt.views)
+print("Views: " + str(yt.views))
+print("Views: " + str(yt.views))
 
 # tipp: when you run python3 youtubeDownloader.py yt."(link)", make sure of the "yt"!!
+
+# Problem solved: 
 
 yd = yt.streams.get_highest_resolution()
 
 yd.download("C:\\Users\\tarsi\\Downloads\\YoutTubeDownloads")
+
+# sources:
+
+# YouTube Video with the tutorial
+# https://www.youtube.com/watch?v=vEQ8CXFWLZU&list=PLsCTnBEpCwH-MZja6wejVZ6sdMbghuGYd&index=4&t=552s
+# 
+# Problems solved: python -m pip install --upgrade pytube (on terminal)
+# https://stackoverflow.com/questions/68956373/i-was-making-a-youtube-video-downloader-but-pytube-is-giving-error
+
+
+
+# where u stopped: tempCodeRunnerFile keeps apearing. how to remove? -> please google
